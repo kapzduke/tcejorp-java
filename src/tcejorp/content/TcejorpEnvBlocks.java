2e122fd312e6b3a41bb72763a4b14894a1c29a21
kapzduke <60543005+kapzduke@users.noreply.github.com>
2022-09-16 21:21:52 +0300

    Update TcejorpEnvBlocks.java

--- src/tcejorp/content/TcejorpEnvBlocks.java
+++ src/tcejorp/content/TcejorpEnvBlocks.java
@@ -13,17 +13,19 @@ public class TcejorpEnvBlocks {
 
 	public void load() {
 
+		//unnamed planet 1
 		turquosia = new Floor("turquosia") {{
-		attributes.set(Attribute.water, -0.1f);
+			variants = 4
+		        attributes.set(Attribute.water, -0.1f);
 			mapColor = Color.valueOf("34827C");
 		}};
 		afinium = new Floor("afinium") {{
+			variants = 4
 			mapColor = Color.valueOf("5490FF");
 		}};
 		turquosiaCraters = new Floor("turquosia-crater") {{
 			attributes.set(Attribute.water, -0.1f);
 			mapColor = Color.valueOf("307A74");
-		}};
 		}};
 		turquosiaWall = new StaticWall("turquosia-wall") {{
 			mapColor = Color.valueOf("57AD98");

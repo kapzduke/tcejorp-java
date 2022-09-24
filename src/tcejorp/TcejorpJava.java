f5538391066c9771f75f488664cb2c647b83807a
kapzduke <60543005+kapzduke@users.noreply.github.com>
2022-09-24 12:39:16 +0300

    Create TcejorpJava.java

---
+++ src/tcejorp/TcejorpJava.java
@@ -0,0 +1,42 @@
+package tcejorp;
+
+import arc.struct.Seq;
+import mindustry.Vars;
+import mindustry.mod.*;
+import tcejorp.content.TcejorpContent;
+import rhino.ImporterTopLevel;
+import rhino.NativeJavaPackage;
+
+//stolen from null
+public class TcejorpJava extends Mod{
+    public TcejorpJava(){}
+
+    @Override
+    public void loadContent() {
+        new TcejorpEnvBlocks().load();
+        new TcejorpItems().load()
+    }
+
+    //stolen from sh1p
+    public static NativeJavaPackage p = null;
+
+    @Override
+    public void init() {
+        super.init();
+        ImporterTopLevel scope = (ImporterTopLevel) Vars.mods.getScripts().scope;
+
+        Seq<String> packages = Seq.with(
+                "tcejorp-java",
+                "tcejorp-java.envblocks",
+                "tcejorp-java.items"
+        );
+
+        packages.each(name -> {
+            p = new NativeJavaPackage(name, Vars.mods.mainLoader());
+
+            p.setParentScope(scope);
+
+            scope.importPackage(p);
+        });
+    }
+}

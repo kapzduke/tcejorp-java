346290e2e6a6d7f4ef6e31ecc19e3b9a7390b8be
kapzduke <60543005+kapzduke@users.noreply.github.com>
2022-09-24 16:33:45 +0300

    Update TcejorpJava.java

--- src/tcejorp/TcejorpJava.java
+++ src/tcejorp/TcejorpJava.java
@@ -4,10 +4,7 @@ import arc.struct.Seq;
 import mindustry.Vars;
 import mindustry.mod.*;
 import tcejorp.content;
-import rhino.ImporterTopLevel;
-import rhino.NativeJavaPackage;
 
-//stolen from null
 public class TcejorpJava extends Mod{
     public TcejorpJava(){}
 
@@ -15,6 +12,5 @@ public class TcejorpJava extends Mod{
     public void loadContent() {
         new TcejorpEnvBlocks().load();
         new TcejorpItems().load();
-        }
     }
 }

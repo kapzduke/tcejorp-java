ebd875b2e0e951a450781ecb8d276838a430425c
kapzduke <60543005+kapzduke@users.noreply.github.com>
2022-09-28 23:59:44 +0300

    Update TcejorpJava.java

--- src/tcejorp/TcejorpJava.java
+++ src/tcejorp/TcejorpJava.java
@@ -1,16 +1,17 @@
 package tcejorp;
 
-import arc.struct.Seq;
-import mindustry.Vars;
 import mindustry.mod.*;
-import tcejorp.content;
+import tcejorp.content.*;
 
 public class TcejorpJava extends Mod{
-    public TcejorpJava(){}
 
-    @Override
-    public void loadContent() {
-        new TcejorpEnvBlocks().load();
-        new TcejorpItems().load();
-    }
+  public TcejorpJava(){
+
+  }
+
+  @Override
+  public void loadContent(){
+    TcejorpItems.load();
+    TcejorpEnvBlocks.load();
+  }
 }
